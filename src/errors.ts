6e84f1cc86337fad0890ec90022dfffd496864c5
Everton Fernande <evertonfrnds@gmail.com>
2023-10-24 20:52:16 -0300

    add default values

--- src/errors.ts
+++ src/errors.ts
@@ -13,64 +13,64 @@ export class PommeError {
 }
 
 const clientErrors = {
-  badRequest: (message: string) => {
+  badRequest: (message: string = 'Bad request') => {
     throw new PommeError(message, 400);
   },
-  unauthorized: (message: string) => {
+  unauthorized: (message: string = 'Unauthorized') => {
     throw new PommeError(message, 401);
   },
-  forbidden: (message: string) => {
+  forbidden: (message: string = 'Forbidden') => {
     throw new PommeError(message, 403);
   },
-  notFound: (message: string) => {
+  notFound: (message: string = 'Not found') => {
     throw new PommeError(message, 404);
   },
-  conflict: (message: string) => {
+  conflict: (message: string = 'Conflict') => {
     throw new PommeError(message, 409);
   },
-  tooManyRequests: (message: string) => {
+  tooManyRequests: (message: string = 'Too many requests') => {
     throw new PommeError(message, 429);
   },
 };
 
 const serverErrors = {
-  serverError: (message: string) => {
+  serverError: (message: string = 'Internal server error') => {
     throw new PommeError(message, 500);
   },
-  notImplemented: (message: string) => {
+  notImplemented: (message: string = 'Not implemented') => {
     throw new PommeError(message, 501);
   },
-  badGateway: (message: string) => {
+  badGateway: (message: string = 'Bad gateway') => {
     throw new PommeError(message, 502);
   },
-  serviceUnavailable: (message: string) => {
+  serviceUnavailable: (message: string = 'Service unavailable') => {
     throw new PommeError(message, 503);
   },
-  gatewayTimeout: (message: string) => {
+  gatewayTimeout: (message: string = 'Gateway timeout') => {
     throw new PommeError(message, 504);
   },
-  httpVersionNotSupported: (message: string) => {
+  httpVersionNotSupported: (message: string = 'HTTP version not supported') => {
     throw new PommeError(message, 505);
   },
 };
 
 const redirectErrors = {
-  permanentRedirect: (message: string) => {
+  permanentRedirect: (message: string = 'Permanent redirect') => {
     throw new PommeError(message, 308);
   },
-  temporaryRedirect: (message: string) => {
+  temporaryRedirect: (message: string = 'Temporary redirect') => {
     throw new PommeError(message, 307);
   },
-  seeOther: (message: string) => {
+  seeOther: (message: string = 'See other') => {
     throw new PommeError(message, 303);
   },
-  notModified: (message: string) => {
+  notModified: (message: string = 'Not modified') => {
     throw new PommeError(message, 304);
   },
-  useProxy: (message: string) => {
+  useProxy: (message: string = 'Use proxy') => {
     throw new PommeError(message, 305);
   },
-  switchProxy: (message: string) => {
+  switchProxy: (message: string = 'Switch proxy') => {
     throw new PommeError(message, 306);
   },
 };
@@ -79,10 +79,11 @@ const error = {
   ...clientErrors,
   ...serverErrors,
   ...redirectErrors,
-  custom: ({
-    message,
-    statusCode = 500,
-  }: PommeErrorArgs & { statusCode?: number }) => {
+  custom: (
+    { message, statusCode = 500 }: PommeErrorArgs & { statusCode?: number } = {
+      message: 'Internal server error',
+    },
+  ) => {
     throw new PommeError(message, statusCode);
   },
 };
